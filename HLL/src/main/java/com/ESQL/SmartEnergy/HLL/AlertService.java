196138fcc5585086b3dbc3ace18ee3c366ee0349
정채리 <165640490+jchaerry@users.noreply.github.com>
2024-11-23 19:24:05 +0900

    fix: DB 비밀번호 삭제

--- HLL/src/main/java/com/ESQL/SmartEnergy/HLL/AlertService.java
+++ HLL/src/main/java/com/ESQL/SmartEnergy/HLL/AlertService.java
@@ -7,6 +7,8 @@ import java.util.Scanner;
 public class AlertService {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/energy_management?useUnicode=true&characterEncoding=utf8";
     private static final String USER = "root";
+    private static final String PASSWORD = ""; // password
+
 
     public void createAlertTable() {
         try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASSWORD)) {
